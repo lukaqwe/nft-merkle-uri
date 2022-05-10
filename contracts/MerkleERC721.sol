af58c507a3bc8873b2bd43c2c9a0f99fa2b92abe
alexeluca <alexeluca.spataru@gmail.com>
2022-05-10 12:09:38 +0300

    Improve comments for docgen

--- contracts/MerkleERC721.sol
+++ contracts/MerkleERC721.sol
@@ -12,7 +12,7 @@ import "./interfaces/IMerkleERC721.sol";
 /// @title NFT contract where the uri is set according to a merkle proof
 contract MerkleERC721 is IMerkleERC721, ERC721URIStorage, Ownable{
 
-    /// @dev can be set only by the contract owner
+    /// @dev Merkle root used for validating proofs, can be set only by the contract owner
     bytes32 internal _merkleRoot; 
     
     // --  8 + 120 bits --
@@ -23,46 +23,63 @@ contract MerkleERC721 is IMerkleERC721, ERC721URIStorage, Ownable{
     /// @dev tokenId to bolean mapping expressing if the corresponding token was already revealed
     mapping (uint256 => bool) internal _tokenRevealed; 
 
+    /**
+     * @dev Modifier to allow transaction only when reveal is enabled
+     */
     modifier whenRevealEnabled(){
         require(_revealEnabled, "Reveal is enabled!");
         _;
     }
 
+    /**
+     * @dev Modifier to allow transaction only when reveal is disabled
+     */
     modifier whenRevealDisabled(){
         require(!_revealEnabled, "Reveal is disabled!");
         _;
     }
 
-    /// @param tokenID was not previously revealed 
+    /**
+     * @dev Modifier to allow reveal if it wasn't performed before
+     * @param tokenID Token to query 
+     */
     modifier whenNotAlreadySet(uint256 tokenID){
         require(!_tokenRevealed[tokenID], "Merkle root already set!");
         _;
     }
 
-    /// @param tokenID only token owner
+    /**
+     * @dev Modifier to allow the transaction only to the token owner
+     * @param tokenID Owner of this token
+     */
     modifier onlyTokenOwner(uint256 tokenID){
         require(msg.sender == ownerOf(tokenID), "You are not the token owner!");
         _;
     }
     
-    /// @param maxSupply maximum supply of nft tokens, cannot be changed later
+    /**
+     * @dev Constructor sets the max supply and puts reveal disabled by defaults
+     * @param maxSupply Maximum supply of nft tokens, cannot be changed later
+     */
     constructor(uint120 maxSupply) ERC721("Random", "RND") Ownable(){
         _maxSupply = maxSupply;
         _revealEnabled = false;
     }
 
-    /// @param to address where the new nft will be minted
-    /// @param tokenID id of the new toke, must not exist already
+    /**
+     * @dev Function to mint a token
+     * @param to Address where the new nft will be minted
+     * @param tokenID Id of the new token, must not exist already
+     */
     function mintNFT(address to, uint256 tokenID) external onlyOwner{
         require(_maxSupply > 0, "Max supply reached");
         _mint(to, tokenID);
         _maxSupply--;
     }
 
-    /// @dev fails when the proof is not valid
-    /// @param tokenID token for which the uri is set
-    /// @param newURI uri string that is set upon a valid proof
-    /// @param proof bytes32 array expressing the path to the merkle root
+    /**
+     * @inheritdoc IMerkleERC721
+     */
     function reveal(uint256 tokenID, string calldata newURI, bytes32[] calldata proof ) external override 
                                                     onlyTokenOwner(tokenID) whenNotAlreadySet(tokenID) whenRevealEnabled{
         bytes32 digest = _calculateHash(tokenID, newURI);
@@ -75,14 +92,18 @@ contract MerkleERC721 is IMerkleERC721, ERC721URIStorage, Ownable{
         emit UpdatedURI(tokenID, newURI);
     }
 
-    /// @param root the new bytes32 merkle root
+    /**
+     * @inheritdoc IMerkleERC721
+     */
     function setMerkleRoot(bytes32 root) external override onlyOwner whenRevealDisabled{
         _merkleRoot = root;
 
         emit MerkleRootUpdated(root);      
     }
 
-    /// @dev merkle root cannot be modified after reveal is enabled
+    /**
+     * @inheritdoc IMerkleERC721
+     */
     function enableReveal() external override onlyOwner whenRevealDisabled{
         require(_merkleRoot != bytes32(0), "Merkle root not set!");
         _revealEnabled = true;
@@ -90,18 +111,24 @@ contract MerkleERC721 is IMerkleERC721, ERC721URIStorage, Ownable{
         emit RevealEnabled();
     }
     
-    /// @return bool true if reveal is enabled
+    /**
+     * @inheritdoc IMerkleERC721
+     */
     function revealEnabled() external view override returns(bool){
         return _revealEnabled;
     }
 
-    /// @return bytes32 root of the merkle tree
+    /**
+     * @inheritdoc IMerkleERC721
+     */ 
     function getMerkleRoot() external view override returns(bytes32) {
         return _merkleRoot;
     }
 
-    /// @dev used to compute the leaf of the merkle tree
-    /// @return bytes32 digest of the keccack function over the contract address, chainId, tokenId, url
+    /**
+     * @dev used to compute the leaf of the merkle tree
+     * @return bytes32 digest of the keccack function over the contract address, chainId, tokenId, url
+     */
     function _calculateHash(uint256 tokenID, string memory url) internal view virtual returns (bytes32) {
         return
             keccak256(
--- contracts/interfaces/IMerkleERC721.sol
+++ contracts/interfaces/IMerkleERC721.sol
@@ -7,22 +7,56 @@ pragma solidity ^0.8.0;
 interface IMerkleERC721{
 
     // ------------- Events ------------- //
+    
+    /**
+     * @dev Emitted when the setMerkleRoot function is called
+     * @param newMerkleRoot The merkle root
+     */
     event MerkleRootUpdated(bytes32 indexed newMerkleRoot);
-    event UpdatedURI(uint256 indexed tokenID, string indexed urI);
+
+    /** 
+     * @dev Emitted when the reveal function is called
+     * @param tokenID Token that has been revealed
+     * @param uri The uri address that is set
+     */
+    event UpdatedURI(uint256 indexed tokenID, string indexed uri);
+
+    /**
+     * @dev Emitted when reveal is enabled by the contract owner
+    */
     event RevealEnabled();
 
 
     // ------------- Methods ------------- //
+
+    /**
+     * @dev Function that sets the new uri for a specific token, fails when the merkle proof is not valid
+     * @param tokenID Token for which the uri is set
+     * @param newURI Uri string that is set upon a valid proof
+     * @param proof Bytes array expressing the path to the merkle root
+    */
     function reveal(uint256 tokenID, string memory uri, bytes32[] calldata proof) external;
 
-    function enableReveal() external;
-
-    function revealEnabled() external view returns(bool);
-
+    /**  
+     * @dev Sets the merkle root, performed only by the contract owner
+     * @param root The new bytes32 merkle root
+     */
     function setMerkleRoot(bytes32 root) external;
 
+    /**
+     * @dev Enables revealing, performed only by the contract owner, merkle root cannot be modified after
+     */
+    function enableReveal() external;
+
+    /**
+     * @dev Getter function to see if reveal is enabled
+     * @return bool True if reveal is enabled
+     */
+    function revealEnabled() external view returns(bool);
+
+    /**
+     * @dev Getter function for the merkle root
+     * @return bytes32 Root of the merkle tree
+     */
     function getMerkleRoot() external view returns(bytes32);
-
-
-
 }
